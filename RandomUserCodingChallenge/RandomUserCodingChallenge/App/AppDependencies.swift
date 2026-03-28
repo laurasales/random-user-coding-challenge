aec40ae76c5cbec748c325d1de13d59cb1c4f391
Laura <saleslaura20@gmail.com>
2026-03-29 00:58:14 +0100

    feat: set up project structure with Clean Architecture folder layout

---
+++ RandomUserCodingChallenge/RandomUserCodingChallenge/App/AppDependencies.swift
@@ -0,0 +1,12 @@
+//
+//  AppDependencies.swift
+//  RandomUserCodingChallenge
+//
+//  Created by Laura Sales Martínez on 29/3/26.
+//
+
+import Foundation
+
+@MainActor
+final class AppDependencies {
+}
